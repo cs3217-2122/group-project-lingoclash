eccba5af72115b3be3997d230bc7ca3f488de44d
Kar Wi <t.karwi@yahoo.com>
2022-03-07 00:37:30 +0800

    Add more theme colors

--- LingoClash/LingoClash/Resources/Theme.swift
+++ LingoClash/LingoClash/Resources/Theme.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class Theme {
     static let mainFontName = "SF-Pro"
-    static let accent = UIColor(named: "AccentColor")
-    static let background = UIColor(named: "BackgroundColor")
-    static let tint = UIColor(named: "TintColor")
-    static let green = UIColor(named: "GreenColor")
-    static let red = UIColor(named: "RedColor")
-    static let secondaryText = UIColor(named: "SecondaryTextColor")
+    static let accent = UIColor(named: "Accent")
+    static let background = UIColor(named: "Background")
+    static let tint = UIColor(named: "Tint")
+    static let green = UIColor(named: "Green")
+    static let red = UIColor(named: "Red")
+    
+    static let primary = UIColor(named: "Primary")
+    static let secondary = UIColor(named: "Secondary")
+    static let tertiary = UIColor(named: "Tertiary")
+    static let error = UIColor(named: "Error")
+    static let primaryText = UIColor(named: "PrimaryText")
+    static let secondaryText = UIColor(named: "SecondaryText")
+    static let tertiaryText = UIColor(named: "TertiaryText")
+    static let errorText = UIColor(named: "ErrorText")
 }
