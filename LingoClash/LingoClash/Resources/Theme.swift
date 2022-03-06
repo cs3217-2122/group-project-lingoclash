82fd7fa445b9bf891619d4bec243fff963285332
Kar Wi <t.karwi@yahoo.com>
2022-03-06 21:12:30 +0800

    Setup font and color theme

---
+++ LingoClash/LingoClash/Resources/Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Theme.swift
+//  LingoClash
+//
+//  Created by Kyle キラ on 6/3/22.
+//
+
+import UIKit
+
+class Theme {
+    static let mainFontName = "SF-Pro"
+    static let accent = UIColor(named: "AccentColor")
+    static let background = UIColor(named: "BackgroundColor")
+    static let tint = UIColor(named: "TintColor")
+    static let green = UIColor(named: "GreenColor")
+    static let red = UIColor(named: "RedColor")
+    static let secondaryText = UIColor(named: "SecondaryTextColor")
+}
