d0a209b2de09d56c95aa2fdc686ac6a88a92fab8
sherrpass <sherwinpoh3@gmail.com>
2022-03-23 13:26:55 +0800

    Rename to lessonSelectorTableCell

---
+++ LingoClash/LingoClash/LessonView/LessonSelection/LessonSelectorTableCell.swift
@@ -0,0 +1,41 @@
+//
+//  LessonSelectorTableCell.swift
+//  LingoClash
+//
+//  Created by Sherwin Poh on 23/3/22.
+//
+
+import UIKit
+
+class LessonSelectorTableCell: UITableViewCell {
+    typealias VM = LessonTableCellViewModel
+    static let reuseIdentifier = "lessonSelectorTableCell"
+
+    @IBOutlet weak var levelNameLabel: UILabel!
+    @IBOutlet var stars: [StarView]!
+    
+    var levelName: String = "" {
+        didSet {
+            self.levelNameLabel.text = levelName
+        }
+    }
+    
+    var viewModel: VM? {
+        didSet {
+            fillUI()
+        }
+    }
+    
+    func fillUI() {
+        guard let viewModel = viewModel else {
+            return
+        }
+        viewModel.starsFilled.enumerated().forEach { stars[$0.offset].isFilled = $0.element }
+        self.levelNameLabel.text = viewModel.lessonName
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+    }
+}
