feef4b06196a516acf28c07803b555db47a903f6
Ai Ling <hong-ailing@hotmail.com>
2022-04-08 17:08:17 +0800

    Style explore books card

---
+++ LingoClash/LingoClash/HomeView/ExploreBookViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ExploreBookViewController.swift
+//  LingoClash
+//
+//  Created by Ai Ling Hong on 8/4/22.
+//
+
+import UIKit
+
+class ExploreBookViewController: UIViewController {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+    }
+    
+    func setUpView() {
+        ViewUtilities.styleCard(containerView)
+    }
+}
