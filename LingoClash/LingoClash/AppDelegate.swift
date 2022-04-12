cdc51fa0708764c497b334338b9d3490bec5dc2c
Kar Wi <t.karwi@yahoo.com>
2022-04-13 00:07:50 +0800

    Improve profile text

--- LingoClash/LingoClash/AppDelegate.swift
+++ LingoClash/LingoClash/AppDelegate.swift
@@ -52,7 +52,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     private func setUpView() {
         UITabBar.appearance().barTintColor = .black
-        UITabBar.appearance().tintColor = .red
+        UITabBar.appearance().tintColor = Theme.current.tint
     }
     
     private func setUpTheme() {
