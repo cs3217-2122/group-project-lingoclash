bede1c6556b29c61d162435d9d846d8e512f17aa
Kar Wi <t.karwi@yahoo.com>
2022-04-13 02:52:32 +0800

    Improve UI for book card

--- LingoClash/LingoClash/HomeView/CurrentBookViewController.swift
+++ LingoClash/LingoClash/HomeView/CurrentBookViewController.swift
@@ -11,6 +11,7 @@ import Combine
 class CurrentBookViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
+    // TODO: remove below
     @IBOutlet weak var bookNameLabel: UILabel!
     @IBOutlet weak var progressLabel: UILabel!
     
