ff9ad01e69294e62755ef50006e089888f94bfc1
Kar Wi <t.karwi@yahoo.com>
2022-04-12 18:10:17 +0800

    Group all storyboards into a folder

---
+++ LingoClash/LingoClash/Resources/Theme/LightTheme.swift
@@ -0,0 +1,15 @@
+//
+//  LightTheme.swift
+//  LingoClash
+//
+//  Created by Kyle キラ on 12/4/22.
+//
+
+import UIKit
+
+class LightTheme: ThemeProtocol {
+    var mainFontName: String = "SF-Pro"
+    var accent: UIColor = UIColor(named: "AccentColor")!
+    var background: UIColor = UIColor(named: "Background")!
+    var tint: UIColor = UIColor(named: "Tint")!
+}
---
+++ LingoClash/LingoClash/Resources/Theme/ThemeProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeProtocol.swift
+//  LingoClash
+//
+//  Created by Kyle キラ on 12/4/22.
+//
+
+import UIKit
+
+
+protocol ThemeProtocol {
+    var mainFontName: String { get }
+    var accent: UIColor { get }
+    var background: UIColor { get }
+    var tint: UIColor { get }
+}
