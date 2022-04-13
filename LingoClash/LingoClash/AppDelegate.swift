f6a0e9d4cc0aa71d97fd2074310bc33f028ea502
Kar Wi <t.karwi@yahoo.com>
2022-04-13 16:15:32 +0800

    Fix home page constraints

--- LingoClash/LingoClash/AppDelegate.swift
+++ LingoClash/LingoClash/AppDelegate.swift
@@ -51,6 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     private func setUpView() {
+        UITabBar.appearance().barTintColor = Theme.current.primaryText
         UITabBar.appearance().tintColor = Theme.current.primary
     }
     
